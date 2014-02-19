7e9e00ec55c1e29af7d4943c969cf85a5acc3db9
Jim Porter <jporter@mozilla.com>
2014-02-19 00:39:58 -0600

    Parameterize the formatter further to support different char types (and traits)

--- io.hpp
+++ io.hpp
@@ -3,13 +3,15 @@
 
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace io {
 
-class formatter {
+template<typename T>
+class basic_formatter {
 public:
-  formatter(const std::string &fmt) {
+  basic_formatter(const T &fmt) {
     size_t start = 0;
     for(size_t i = 0; i < fmt.size(); i++) {
       // Jesus Christ how horrifying
@@ -23,98 +25,158 @@ public:
     strings_.push_back(fmt.substr(start));
   }
 
-  std::vector<std::string> strings_;
+  std::vector<T> strings_;
   std::vector<size_t> placeholders_;
 };
+typedef basic_formatter<char> formatter;
 
-template<typename T>
+template<typename T, typename String = std::string>
 class has_tostring {
+  // It might be possible to simplify this, but it works.
   template<typename U>
-  static constexpr auto has_tostring_(int, int, int) -> decltype(
+  static constexpr auto has_tostring_(int, int, int, int, int, int) -> decltype(
+      tostring<String>(std::declval<U>(), std::declval<const char*>()),
+      tostring<String>(std::declval<U>()),
+      int()) {
+    return 4 | 2 | 1;
+  }
+  template<typename U>
+  static constexpr auto has_tostring_(int, int, int, int, int, ...) -> decltype(
       tostring(std::declval<U>(), std::declval<const char*>()),
       tostring(std::declval<U>()),
       int()) {
     return 2 | 1;
   }
+
   template<typename U>
-  static constexpr auto has_tostring_(int, int, ...) -> decltype(
+  static constexpr auto has_tostring_(int, int, int, int, ...) -> decltype(
+      tostring<String>(std::declval<U>(), std::declval<const char*>()),
+      int()) {
+    return 4 | 2;
+  }
+  template<typename U>
+  static constexpr auto has_tostring_(int, int, int, ...) -> decltype(
       tostring(std::declval<U>(), std::declval<const char*>()),
       int()) {
     return 2;
   }
+
+  template<typename U>
+  static constexpr auto has_tostring_(int, int, ...) -> decltype(
+    String(tostring<String>(std::declval<U>())),
+      int()) {
+    return 4 | 1;
+  }
   template<typename U>
   static constexpr auto has_tostring_(int, ...) -> decltype(
-      tostring(std::declval<U>()),
+    String(tostring(std::declval<U>())),
       int()) {
     return 1;
   }
+
   template<typename U>
   static constexpr auto has_tostring_(...) -> int {
     return 0;
   }
 public:
-  enum { value = has_tostring_<T>(0, 0, 0) };
+  enum { value = has_tostring_<T>(0, 0, 0, 0, 0, 0) };
 };
 
 namespace detail {
-  template<typename T>
-  inline typename std::enable_if<has_tostring<T>::value == 0, void>::type
-  try_format(std::ostream &o, T &&t) {
-    o << "error!";
+  // First, some helpers to call tostring whether or not it takes a template to
+  // define the return type.
+  template<typename String, typename T>
+  inline auto tostring_(T &&t) -> typename std::enable_if<
+    (has_tostring<T, String>::value & 4) == 0,
+    decltype(tostring( std::declval<T>() ))
+  >::type {
+    return tostring(std::forward<T>(t));
   }
 
-  template<typename T>
-  inline typename std::enable_if<has_tostring<T>::value & 1, void>::type
-  try_format(std::ostream &o, T &&t) {
-    o << tostring(t);
+  template<typename String, typename T>
+  inline auto tostring_(T &&t) -> typename std::enable_if<
+    has_tostring<T, String>::value & 4,
+    decltype(tostring<String>( std::declval<T>() ))
+  >::type {
+    return tostring<String>(std::forward<T>(t));
   }
 
-  template<typename T>
-  inline typename std::enable_if<has_tostring<T>::value == 2, void>::type
-  try_format(std::ostream &o, T &&t) {
-    o << tostring(t, "");
+  // Next, some helpers to delegate to the proper tostring based on whether
+  // format args were provided and whether tostring accepts them.
+  template<typename Char, typename Traits, typename T>
+  inline typename std::enable_if<
+    has_tostring<T, std::basic_string<Char, Traits>>::value == 0, void
+  >::type
+    try_format(std::basic_ostream<Char, Traits> &o, T &&t) {
+    throw "error!";
   }
 
-  template<typename T>
-  inline typename std::enable_if<has_tostring<T>::value == 0, void>::type
-  try_format(std::ostream &o, T &&t, const char *fmt) {
-    o << "error!";
+  template<typename Char, typename Traits, typename T>
+  inline typename std::enable_if<
+    has_tostring<T, std::basic_string<Char, Traits>>::value & 1, void
+  >::type
+  try_format(std::basic_ostream<Char, Traits> &o, T &&t) {
+    o << tostring_<std::basic_string<Char, Traits>>(t);
   }
 
-  template<typename T>
-  inline typename std::enable_if<has_tostring<T>::value == 1, void>::type
-  try_format(std::ostream &o, T &&t, const char *fmt) {
-    o << tostring(t);
+  template<typename Char, typename Traits, typename T>
+  inline typename std::enable_if<
+    (has_tostring<T, std::basic_string<Char, Traits>>::value & 3) == 2, void
+  >::type
+  try_format(std::basic_ostream<Char, Traits> &o, T &&t) {
+    o << tostring_<std::basic_string<Char, Traits>>(t, "");
   }
 
-  template<typename T>
-  inline typename std::enable_if<has_tostring<T>::value & 2, void>::type
-  try_format(std::ostream &o, T &&t, const char *fmt) {
-    o << tostring(t, fmt);
+  template<typename Char, typename Traits, typename T>
+  inline typename std::enable_if<
+    has_tostring<T, std::basic_string<Char, Traits>>::value == 0, void
+  >::type
+  try_format(std::basic_ostream<Char, Traits> &o, T &&t, const char *fmt) {
+    throw "error!";
   }
 
-  template<typename ...Rest>
-  inline void format_one(std::ostream &o, int i, Rest &&...args) {
+  template<typename Char, typename Traits, typename T>
+  inline typename std::enable_if<
+    (has_tostring<T, std::basic_string<Char, Traits>>::value & 3) == 1, void
+  >::type
+  try_format(std::basic_ostream<Char, Traits> &o, T &&t, const char *fmt) {
+    o << tostring_<std::basic_string<Char, Traits>>(t);
+  }
+
+  template<typename Char, typename Traits, typename T>
+  inline typename std::enable_if<
+    has_tostring<T, std::basic_string<Char, Traits>>::value & 2, void
+  >::type
+  try_format(std::basic_ostream<Char, Traits> &o, T &&t, const char *fmt) {
+    o << tostring_<std::basic_string<Char, Traits>>(t, fmt);
+  }
+
+  // Finally, some helpers to return a formatted version of the i'th argument
+  // in a template parameter pack.
+  template<typename Char, typename Traits, typename ...Rest>
+  inline void format_one(std::basic_ostream<Char, Traits> &o, int i,
+                         Rest &&...args) {
     // This should never actually be called.
   }
 
-  template<typename T, typename ...Rest>
-  inline void format_one(std::ostream &o, int i, T &&x, Rest &&...args) {
+  template<typename Char, typename Traits, typename T, typename ...Rest>
+  inline void format_one(std::basic_ostream<Char, Traits> &o, int i, T &&x,
+                         Rest &&...args) {
     if(i == 0)
       try_format(o, x);
     else
       format_one(o, i-1, args...);
   }
 
-  template<typename ...Rest>
-  inline void format_one(std::ostream &o, const char *fmt, int i,
-                         Rest &&...args) {
+  template<typename Char, typename Traits, typename ...Rest>
+  inline void format_one(std::basic_ostream<Char, Traits> &o, const char *fmt,
+                         int i, Rest &&...args) {
     // This should never actually be called.
   }
 
-  template<typename T, typename ...Rest>
-  inline void format_one(std::ostream &o, const char *fmt, int i, T &&x,
-                         Rest &&...args) {
+  template<typename Char, typename Traits, typename T, typename ...Rest>
+  inline void format_one(std::basic_ostream<Char, Traits> &o, const char *fmt,
+                         int i, T &&x, Rest &&...args) {
     if(i == 0)
       try_format(o, x, fmt);
     else
@@ -122,13 +184,24 @@ namespace detail {
   }
 }
 
-template<typename ...T>
-void format(std::ostream &o, const std::string &fmt, T &&...args) {
-  format(o, formatter(fmt), args...);
+// Not sure why I need this overload... you'd think the next one would be
+// deduceable.
+template<typename Char, typename Traits, typename ...T>
+inline void format(std::basic_ostream<Char, Traits> &o, const Char *fmt,
+                   T &&...args) {
+  format(o, basic_formatter<std::basic_string<Char, Traits>>(fmt), args...);
 }
 
-template<typename ...T>
-void format(std::ostream &o, const formatter &fmt,
+template<typename Char, typename Traits, typename ...T>
+inline void format(std::basic_ostream<Char, Traits> &o,
+                   const std::basic_string<Char, Traits> &fmt,
+                   T &&...args) {
+  format(o, basic_formatter<std::basic_string<Char, Traits>>(fmt), args...);
+}
+
+template<typename Char, typename Traits, typename ...T>
+void format(std::basic_ostream<Char, Traits> &o,
+            const basic_formatter<std::basic_string<Char, Traits>> &fmt,
             T &&...args) {
   auto str = fmt.strings_.begin();
   for(auto i : fmt.placeholders_) {
--- test.cpp
+++ test.cpp
@@ -3,21 +3,37 @@
 
 // For some reason, these need to be declared before we include io.hpp, which
 // makes sense really (but is annoying).
-std::string tostring(const std::string &);
+//std::string tostring(const std::string &);
+template<typename T = std::string>
+T tostring(const std::string &s) {
+  return s;
+}
+
+template<>
+std::wstring tostring(const std::string &s) {
+  return std::wstring(L"wide");
+}
+
 std::string tostring(int);
+std::wstring tostring(double);
 
 #include "io.hpp"
 
-std::string tostring(const std::string &s) {
-  return s;
-}
 std::string tostring(int i) {
   return std::to_string(i);
+}
+std::wstring tostring(double i) {
+  return std::to_wstring(i);
 }
 
 int main() {
   io::format(std::cout,
              "First we have {1}; wait, before that was {0}. Finally, {2}.\n",
              "foo", 1, "bar");
+
+  io::format(std::wcout,
+             L"First we have {1}; wait, before that was {0}. Finally, {2}.\n",
+             "foo", 1.0, "bar");
+
   return 0;
 }
