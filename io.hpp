b962766644d8805924f1f8f05b3486fd65726490
Jim Porter <jporter@mozilla.com>
2014-03-09 16:58:05 -0500

    Implement a proper parser for format strings

--- io.hpp
+++ io.hpp
@@ -1,35 +1,161 @@
-#ifndef INC_IO_HPP
-#define INC_IO_HPP
+#ifndef INC_IODREAMS_IO_HPP
+#define INC_IODREAMS_IO_HPP
 
+#include <cassert>
+#include <cctype>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <utility>
 #include <vector>
 
 namespace io {
 
-template<typename T>
-class basic_formatter {
-public:
-  basic_formatter(const T &fmt) {
-    size_t start = 0;
-    for(size_t i = 0; i < fmt.size(); i++) {
-      // Jesus Christ how horrifying
-      if(fmt[i] == '{') {
-        strings_.push_back(fmt.substr(start, i-start));
-        placeholders_.push_back(fmt[i+1] - '0');
-        i += 2; // Skip the number and the '}'
-        start = i + 1;
-      }
-    }
-    strings_.push_back(fmt.substr(start));
+namespace detail {
+  // Convert an arbitrary string type to an std::string, assuming all code
+  // points are already ASCII.
+  template<typename T>
+  inline std::string charify(const T &s) {
+    return std::string(s.begin(), s.end());
   }
 
-  std::vector<T> strings_;
-  std::vector<size_t> placeholders_;
+  inline std::string charify(const std::string &s) {
+    return s;
+  }
+}
+
+class format_string_error : public std::invalid_argument {
+  using std::invalid_argument::invalid_argument;
 };
-typedef basic_formatter<std::string> formatter;
-typedef basic_formatter<std::wstring> wformatter;
+
+template<typename T>
+class basic_format_string {
+public:
+  typedef T string_type;
+
+  struct placeholder {
+    placeholder(size_t index) : index(index) {}
+    placeholder(size_t index, const std::string &params)
+      : index(index), params(params) {}
+    placeholder(size_t index, std::string &&params)
+      : index(index), params(std::move(params)) {}
+
+    size_t index;
+    std::string params;
+  };
+
+  basic_format_string(const T &fmt) {
+    enum parse_state {
+      START,
+      OPEN_BRACE,
+      CLOSE_BRACE,
+      PH_INDEX,
+      PH_PARAMS
+    };
+
+    parse_state state = START;
+    size_t start = 0, ph_index;
+    string_type working_string;
+    for(size_t i = 0; i < fmt.size(); i++) {
+      switch(state) {
+      case START:
+        if(fmt[i] == '{') {
+          state = OPEN_BRACE;
+          working_string += fmt.substr(start, i - start);
+        }
+        else if(fmt[i] == '}') {
+          state = CLOSE_BRACE;
+          working_string += fmt.substr(start, i - start);
+        }
+        break;
+      case OPEN_BRACE:
+        if(fmt[i] == '{') {
+          state = START;
+          start = i;
+        }
+        else if(std::isdigit(fmt[i])) {
+          state = PH_INDEX;
+          ph_index = fmt[i] - '0';
+        }
+        else if(fmt[i] == '|') {
+          ph_index = 0; // TODO: infer the index in a smarter way
+          state = PH_PARAMS;
+          start = i + 1;
+        }
+        else if(fmt[i] == '}') {
+          ph_index = 0; // TODO: infer the index in a smarter way
+          strings_.push_back(working_string);
+          working_string = string_type();
+          placeholders_.push_back({ ph_index });
+          state = START;
+          start = i + 1;
+        }
+        else {
+          throw format_string_error("unexpected character in placeholder");
+        }
+        break;
+      case CLOSE_BRACE:
+        if(fmt[i] == '}') {
+          state = START;
+          start = i;
+        }
+        else {
+          throw format_string_error("unexpected '}' in string");
+        }
+        break;
+      case PH_INDEX:
+        if(std::isdigit(fmt[i])) {
+          ph_index = ph_index * 10 + fmt[i] - '0';
+        }
+        else if(fmt[i] == '|') {
+          state = PH_PARAMS;
+          start = i + 1;
+        }
+        else if(fmt[i] == '}') {
+          strings_.push_back(working_string);
+          working_string = string_type();
+          placeholders_.push_back({ ph_index });
+          state = START;
+          start = i + 1;
+        }
+        else {
+          throw format_string_error(
+            "unexpected character in placeholder index"
+          );
+        }
+        break;
+      case PH_PARAMS:
+        if(fmt[i] == '}') {
+          strings_.push_back(working_string);
+          working_string = string_type();
+          placeholders_.push_back({
+            ph_index, detail::charify(fmt.substr(start, i - start))
+          });
+          state = START;
+          start = i + 1;
+        }
+        else if(fmt[i] > 127) {
+          throw format_string_error(
+            "unexpected character in placeholder params"
+          );
+        }
+        break;
+      default:
+        assert(false && "invalid parser state");
+      }
+    }
+
+    if(state != START)
+      throw format_string_error("unexpected end of string");
+    working_string += fmt.substr(start);
+    strings_.push_back(working_string);
+  }
+
+  std::vector<string_type> strings_;
+  std::vector<placeholder> placeholders_;
+};
+typedef basic_format_string<std::string> format_string;
+typedef basic_format_string<std::wstring> wformat_string;
 
 template<typename T, typename String, bool Formatted>
 class has_tostring {
@@ -135,23 +261,25 @@ namespace detail {
   }
 
   template<typename String, typename T>
-  inline auto call_tostring(T &&t, const char *fmt) -> typename std::enable_if<
-    !has_tostring<T, String, false>::templated,
-    typename has_tostring<T, String, false>::return_type
+  inline auto call_tostring(T &&t, const char *params) ->
+    typename std::enable_if<
+      !has_tostring<T, String, false>::templated,
+      typename has_tostring<T, String, false>::return_type
   >::type {
     static_assert(has_tostring<T, String, false>::compatible,
                   "tostring function has incompatible return type");
-    return tostring(std::forward<T>(t), fmt);
+    return tostring(std::forward<T>(t), params);
   }
 
   template<typename String, typename T>
-  inline auto call_tostring(T &&t, const char *fmt) -> typename std::enable_if<
-    has_tostring<T, String, false>::templated,
-    typename has_tostring<T, String, false>::return_type
+  inline auto call_tostring(T &&t, const char *params) ->
+    typename std::enable_if<
+      has_tostring<T, String, false>::templated,
+      typename has_tostring<T, String, false>::return_type
   >::type {
     static_assert(has_tostring<T, String, false>::compatible,
                   "tostring function has incompatible return type");
-    return tostring<String>(std::forward<T>(t), fmt);
+    return tostring<String>(std::forward<T>(t), params);
   }
 
   // Next, some helpers to delegate to the proper tostring based on whether
@@ -161,7 +289,7 @@ namespace detail {
     !has_tostring<T, std::basic_string<Char, Traits>, false>::value &&
     !has_tostring<T, std::basic_string<Char, Traits>, true >::value, void
   >::type
-  try_format(std::basic_ostream<Char, Traits> &o, T &&t, const char *fmt = 0) {
+  try_format(std::basic_ostream<Char, Traits> &, T &&, const char *) {
     static_assert(
       !has_tostring<T, std::basic_string<Char, Traits>, false>::value &&
       !has_tostring<T, std::basic_string<Char, Traits>, true >::value,
@@ -171,91 +299,60 @@ namespace detail {
 
   template<typename Char, typename Traits, typename T>
   inline typename std::enable_if<
-    has_tostring<T, std::basic_string<Char, Traits>, false>::value, void
-  >::type
-  try_format(std::basic_ostream<Char, Traits> &o, T &&t) {
-    o << call_tostring<std::basic_string<Char, Traits>>(t);
-  }
-  template<typename Char, typename Traits, typename T>
-  inline typename std::enable_if<
-    !has_tostring<T, std::basic_string<Char, Traits>, false>::value &&
-     has_tostring<T, std::basic_string<Char, Traits>, true >::value, void
-  >::type
-  try_format(std::basic_ostream<Char, Traits> &o, T &&t) {
-    o << call_tostring<std::basic_string<Char, Traits>>(t, "");
-  }
-
-  template<typename Char, typename Traits, typename T>
-  inline typename std::enable_if<
     has_tostring<T, std::basic_string<Char, Traits>, true>::value, void
   >::type
-  try_format(std::basic_ostream<Char, Traits> &o, T &&t, const char *fmt) {
-    o << call_tostring<std::basic_string<Char, Traits>>(t, fmt);
+  try_format(std::basic_ostream<Char, Traits> &o, T &&t, const char *params) {
+    o << call_tostring<std::basic_string<Char, Traits>>(t, params);
   }
   template<typename Char, typename Traits, typename T>
   inline typename std::enable_if<
     !has_tostring<T, std::basic_string<Char, Traits>, true >::value &&
      has_tostring<T, std::basic_string<Char, Traits>, false>::value, void
   >::type
-  try_format(std::basic_ostream<Char, Traits> &o, T &&t, const char *fmt) {
+  try_format(std::basic_ostream<Char, Traits> &o, T &&t, const char *) {
     o << call_tostring<std::basic_string<Char, Traits>>(t);
   }
 
   // Finally, some helpers to return a formatted version of the i'th argument
   // in a template parameter pack.
   template<typename Char, typename Traits, typename ...Rest>
-  inline void format_one(std::basic_ostream<Char, Traits> &o, int i,
-                         Rest &&...args) {
-    // This should never actually be called.
+  inline void format_one(std::basic_ostream<Char, Traits> &, size_t,
+                         const char *, Rest &&...) {
+    assert(false && "format_one() failed somehow!");
   }
 
   template<typename Char, typename Traits, typename T, typename ...Rest>
-  inline void format_one(std::basic_ostream<Char, Traits> &o, int i, T &&x,
-                         Rest &&...args) {
+  inline void format_one(std::basic_ostream<Char, Traits> &o, size_t i,
+                         const char *params, T &&x, Rest &&...args) {
     if(i == 0)
-      try_format(o, x);
+      try_format(o, x, params);
     else
-      format_one(o, i-1, args...);
-  }
-
-  template<typename Char, typename Traits, typename ...Rest>
-  inline void format_one(std::basic_ostream<Char, Traits> &o, const char *fmt,
-                         int i, Rest &&...args) {
-    // This should never actually be called.
-  }
-
-  template<typename Char, typename Traits, typename T, typename ...Rest>
-  inline void format_one(std::basic_ostream<Char, Traits> &o, const char *fmt,
-                         int i, T &&x, Rest &&...args) {
-    if(i == 0)
-      try_format(o, x, fmt);
-    else
-      format_one(o, i-1, args...);
+      format_one(o, i-1, params, args...);
   }
 }
 
 template<typename Char, typename Traits, typename ...T>
 inline void format(std::basic_ostream<Char, Traits> &o, const Char *fmt,
                    T &&...args) {
-  format(o, basic_formatter<std::basic_string<Char, Traits>>(fmt), args...);
+  format(o, basic_format_string<std::basic_string<Char, Traits>>(fmt), args...);
 }
 
 template<typename Char, typename Traits, typename ...T>
 inline void format(std::basic_ostream<Char, Traits> &o,
                    const std::basic_string<Char, Traits> &fmt,
                    T &&...args) {
-  format(o, basic_formatter<std::basic_string<Char, Traits>>(fmt), args...);
+  format(o, basic_format_string<std::basic_string<Char, Traits>>(fmt), args...);
 }
 
 template<typename Char, typename Traits, typename ...T>
 void format(std::basic_ostream<Char, Traits> &o,
-            const basic_formatter<std::basic_string<Char, Traits>> &fmt,
+            const basic_format_string<std::basic_string<Char, Traits>> &fmt,
             T &&...args) {
   auto str = fmt.strings_.begin();
-  for(auto i : fmt.placeholders_) {
+  for(auto ph : fmt.placeholders_) {
     o << *str;
     ++str;
-    detail::format_one(o, i, args...);
+    detail::format_one(o, ph.index, ph.params.c_str(), args...);
   }
   o << *str;
 }
--- test.cpp
+++ test.cpp
@@ -3,36 +3,44 @@
 
 // For some reason, these need to be declared before we include io.hpp, which
 // makes sense really (but is annoying).
-//std::string tostring(const std::string &);
 template<typename T = std::string>
-T tostring(const std::string &s) {
-  return s;
-}
-
-template<>
-std::wstring tostring(const std::string &s) {
-  return std::wstring(L"wide");
-}
+T tostring(const std::string &s, const char *params = 0);
 
 std::string tostring(int);
+
 std::wstring tostring(double);
 
 #include "io.hpp"
 
+template<>
+std::string tostring(const std::string &s, const char *params) {
+  if (params && params[0] == 'q')
+    return "beep";
+  return s;
+}
+
+template<>
+std::wstring tostring(const std::string &, const char *params) {
+  if (params && params[0] == 'q')
+    return L"weep";
+  return std::wstring(L"wide");
+}
+
 std::string tostring(int i) {
   return std::to_string(i);
 }
+
 std::wstring tostring(double i) {
   return std::to_wstring(i);
 }
 
 int main() {
   io::format(std::cout,
-             "First we have {1}; wait, before that was {0}. Finally, {2}.\n",
+             "First we have {1}; wait, before that was {0|q}. Finally, {2}.\n",
              "foo", 1, "bar");
 
   io::format(std::wcout,
-             L"First we have {1}; wait, before that was {0}. Finally, {2}.\n",
+             L"First we have {1}; wait, before that was {0|q}. Finally, {2}.\n",
              "foo", 1.0, "bar");
 
   return 0;
